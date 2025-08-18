69a09f6812eae837af2e48808eb86e4ece323d18
Santiago Rovere <srovere@gmail.com>
2025-08-18 09:05:01 -0300

    Mapas de climatología y anomalías

--- 04b_mapas_datos_grillados.r
+++ 04b_mapas_datos_grillados.r
@@ -12,7 +12,30 @@ load("data/ClimIndVis_Grid.RData")
 # Carga del archivo ClimIndVis generado a partir de datos puntuales
 load("data/ClimIndVis_Stations.RData")
 
-# 1. Generación de mapa de anomalías de días secos
+### 1. MAPAS DE CLIMATOLOGIA
+
+# 1a. Generación de mapa de media te temperatura máxima
+index_args <- list(aggt = "seasonal", var = "tmax")
+plot_args  <- list(p_cex = 5, plwidth = 10, NA_col = "grey50", zlim = c(-30, 30))
+ClimIndVis::autoplot_climatology_map(
+  dat_grid = climindvis_grid, index = "mean", index_args = index_args,
+  selyears = c(2010:2019), plot_args = plot_args, title = "Promedio de temperatura máxima",
+  output = "png", plotdir = "data/", plotname = "mapa_climatologia_estacional"
+)
+
+# 1b. Generación de mapa de precipitación acumulada
+index_args <- list(aggt = "seasonal", var = "prec")
+plot_args  <- list(p_cex = 5, plwidth = 10, NA_col = "grey50", zlim = c(0, 500))
+ClimIndVis::autoplot_climatology_map(
+  dat_grid = climindvis_grid, index = "sum", index_args = index_args,
+  selyears = c(2010:2019), plot_args = plot_args, title = "Precipitación acumulada",
+  output = "png", plotdir = "data/", plotname = "mapa_climatologia_prec"
+)
+
+
+### 2. MAPAS DE ANOMALIAS
+
+# 2a. Generación de mapa de anomalías de días secos
 index_args <- list(aggt = "seasonal", selagg = c("SON"), iformat = "perc")
 plot_args  <- list(p_cex = 5, plwidth = 10, NA_col = "grey50")
 ClimIndVis::autoplot_anomaly_map(
@@ -22,7 +45,16 @@ ClimIndVis::autoplot_anomaly_map(
   output = "png", plotdir = "data/", plotname = "mapa_anomalias_dd"
 )
 
-# 2. Generación de mapa de anomalías de SPI-3
+# 2b. Generación de mapa de anomalías de días secos con datos punduales
+ClimIndVis::autoplot_anomaly_map(
+  dat_grid = climindvis_grid, dat_p = climindvis_st, 
+  index = "dd", index_args = index_args,
+  refyears = c(1991:2000), anomyears = 2008, 
+  plot_args = plot_args, title = "Anomalías de días secos + datos puntuales",
+  output = "png", plotdir = "data/", plotname = "mapa_anomalias_dd_puntuales"
+)
+
+# 2c. Generación de mapa de anomalías de SPI-3
 index_args <- list(timescale = 3, ref = c(1991, 2020), aggt = "monthly")
 plot_args  <- list(p_cex = 5, plwidth = 10, NA_col = "grey50")
 ClimIndVis::autoplot_anomaly_map(
@@ -32,3 +64,4 @@ ClimIndVis::autoplot_anomaly_map(
   output = "png", plotdir = "data/", plotname = "mapa_anomalias_spi_3"
 )
 
+### 3. MAPAS DE TENDENCIAS
